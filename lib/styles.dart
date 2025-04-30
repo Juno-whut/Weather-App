307e6a7773e1026fe9a6104d7ca9423d685ef186
Juno-whut <owenrupnow17@gmail.com>
2025-04-30 18:15:36 -0400

    Made a styles file for all of my text styles

---
+++ lib/styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+// text style for things like wind speed, humidity, etc.
+TextStyle weatherStatsStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 15,
+);
+
+// city name style
+TextStyle titleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 40,
+  fontWeight: FontWeight.w600,
+);
+
+// main temp style
+TextStyle mainConditionStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
